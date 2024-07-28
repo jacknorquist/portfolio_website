5d95b07df3aaf1fe9a0b67d1f1a9ca17ee501c17
Jack Norquist <jack.norquist@outlook.com>
2024-07-28 15:03:37 -0500

    MiniCssExtractPlugin configures w webpack to enable correct css loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
   entry: './src/index.js', // Entry point of your application
@@ -22,6 +24,22 @@ module.exports = {
       },
     ],
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Regex to match .css files
+        use: [
+          MiniCssExtractPlugin.loader, // Extracts CSS into separate files
+          'css-loader', // Processes CSS
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css', // Output file for CSS
+    }),
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
